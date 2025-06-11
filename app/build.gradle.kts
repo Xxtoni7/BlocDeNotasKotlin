2f0d58527a5d19d6e0ba5bc09fc8776d7de379fd
Xxtoni7 <toniwpp8@gmail.com>
2025-06-11 20:11:01 -0300

    Agrego ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation("androidx.navigation:navigation-compose:2.8.7")
-
+    implementation(libs.ktor.client.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
